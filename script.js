183945c8f3b3319af187b6f6245c5dbce0cfa18c
helfuyuki <majorkriegbr@gmail.com>
2025-10-29 22:32:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const tail = document.getElementById('tail');
 const meow = document.getElementById('meow');
 
 tail.addEventListener('click', () => {
-  // anima o rabo
-  tail.classList.add('wagging');
-  setTimeout(() => tail.classList.remove('wagging'), 500);
-
-  // toca o áudio com 50% de volume
-  meow.volume = 0.5;
-  meow.currentTime = 0;
-  meow.play();
+  tail.classList.add('wagging'); // animação do rabo
+  meow.currentTime = 0;          // reinicia o áudio
+  meow.play();                   // toca o áudio
+  setTimeout(() => {
+    tail.classList.remove('wagging');
+  }, 500);
 });
+
